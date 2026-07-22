0e82a65b49ab37238d47d79ddea35f5d311fbd3e
G. Mh. <Veleshu404@gmail.com>
2026-07-22 12:43:21 +0300

    Updated lexer, started creating parser

---
+++ src/parser/parser.js
@@ -0,0 +1,42 @@
+export class Parser{
+    constructor(tokens) {
+        this.tokens = tokens
+        this.position = 0
+        this.structure = []
+    }
+    parse() {
+        let level = 0
+        let token = ""
+        let struct = {}
+        while (this.position < this.tokens.length) {
+            struct = {}
+            token = this.peek()
+            if (token.type == "keyword" && ["load", "goto", "point"].includes(token.value)){
+                this.advance()
+                struct.type = token.value
+                token = this.peek()
+                if (token.type == "string") {
+                    struct.value = token.value
+                    this.structure.push(struct)
+                    console.log(struct)
+                } else {
+                    throw Error("Expected string after '" + struct.type +"', got " + token.type + " instead")
+                }
+            } else {
+                this.advance()
+            }
+        }
+        if (this.position >= this.tokens.length - 1) {return this.tokens}
+    }
+    peek() {
+        return this.tokens[this.position]
+    }
+    advance() {
+        this.position += 1
+    }
+    checkEnd() {
+        return this.position >= this.tokens.length 
+    }
+}
+
+
