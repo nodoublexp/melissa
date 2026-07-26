21475b1c8604dd2a6cea5c5c27f19e7bce37381d
G. Mh. <Veleshu404@gmail.com>
2026-07-26 16:27:35 +0300

    Started creating lexer

---
+++ src/parser/lexer.js
@@ -0,0 +1,123 @@
+export class Lexer {
+    constructor(text) {
+        this.text = text;
+        this.position = 0;
+        this.tokens = [];
+        this.symbols = [
+            "(",")",                // parentheses
+            "[","]",                // brackets
+            "&","|","!",            // logical
+            "=","<",">",            // comparison
+            "+","-","*","/","^",    // math
+            "'",'"',                // strings
+            ":",                    // colon
+            ".",                    // dot
+            ",",                    // comma
+            "\n", "\t",             // special
+            "@","#","$"             // prefixes
+        ]
+        this.keywords = [
+            "set",
+            "choice",
+            "text",
+            "say",
+            "load",
+            "goto",
+            "act",
+            "if",
+            "elif",
+            "else",
+            "in"
+        ]
+        this.expressors = [
+            ">=",
+            "<=",
+            "==",
+            "!=",
+            "&&",
+            "||",
+            "!",
+            ">",
+            "<"
+        ]
+    }
+    tokenize() {
+        this.text = this.text.replace(/    /g, "\t");
+        let char = ""
+        let token = ""
+        
+        while (this.position < this.text.length) {
+            token = ""
+            char = this.peek()
+            if (char == "'" || char == '"') {
+                let indicator = char
+                this.advance()
+                let token = ""
+                while (this.peek() != indicator) {
+                    token += this.peek()
+                    this.advance()
+                }
+                this.advance()
+                this.tokens.push({type: "string", value: token})
+            } else if ("-0123456789".includes(char)){
+                let dotcount = 0
+                token += char
+                this.advance()
+                char = this.peek()
+                while ("0123456789.".includes(char)) {
+                    token += char
+                    if (char == ".") {
+                        dotcount += 1
+                    }
+                    this.advance()
+                    char = this.peek()
+                }
+                if (dotcount > 1) {throw Error("Invalid number '"+token+"'")}
+                if (token == "-") {this.tokens.push({type:"operator", value:token})}
+                else {this.tokens.push({type:"number", value:token})}
+            } else if ("\n\t".includes(char)) {
+                this.advance()
+                this.tokens.push({type:"special", value:char})
+            } else if ("()".includes(char)) {
+                this.advance()
+                this.tokens.push({type:"parenthesis", value:char})
+            } else if ("[]".includes(char)) {
+                this.advance()
+                this.tokens.push({type:"bracket", value:char})
+            } else if ("+*/^".includes(char)) {
+                this.advance()
+                this.tokens.push({type:"math", value:char})
+            } else if (char == " ") {
+                this.advance()
+            } else if ("><!=".includes(char)) {
+                token += char
+                this.advance()
+                char = this.peek()
+                while ("><=".includes(char)) {
+                    this.advance()
+                    token += char
+                    char = this.peek()
+                }
+                if (token == "!") {this.tokens.push({type:"logical", value:token})}
+                else {
+                    if (this.expressors.includes(token)) {
+                        this.tokens.push({type:"comparison", value:token})
+                    } else {
+                        throw Error("Invalid comparison token '"+token+"'")
+                    }
+                }
+            }
+            else {
+                this.advance()
+            }
+            
+            if (this.position >= this.text.length - 1) {return this.tokens}
+        }
+    }
+    advance() {
+        this.position+=1;
+    }
+    peek() {
+        return this.text[this.position]
+    }
+}
